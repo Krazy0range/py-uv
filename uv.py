a41c55d2dfa7e89e4bf8d2ac258a8d8ee795b72c
Krazy0range <teoal@icloud.com>
2024-01-02 18:09:28 -0600

    more refactoring, add remaining time

--- uv.py
+++ uv.py
@@ -8,16 +8,29 @@ from mutagen.mp3 import MP3
 import cursor
 
 width = 120
+uv_folder_path = 'C:\\Users\\Teo\\Documents\\UV'
+
+class InvalidChoice(Exception):
+    pass
+
+class RaiseInvalidChoice():
+    def __enter__(self):
+        pass
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        if exc_type == ValueError:
+            raise InvalidChoice()
+        return False
 
 def end():
     clear()
-    # print('\033[?25h')
     cursor.show()
     mixer.quit()
     sys.exit()
         
 def clear():
     os.system('cls')
+    
+# CHOOSE
         
 def resetLine():
     print(f"\033[A\r{' '*80}\r", end='')
@@ -34,17 +47,6 @@ def print_menu(menu):
     for index, item in enumerate(menu):
         print(f'{even_off if is_even(index) else odd_off}{str(index).ljust(index_width, " ")}{reset_dim}{even if is_even(index) else odd}{item.ljust(width - index_width, " ")}{reset}')
 
-class InvalidChoice(Exception):
-    pass
-
-class RaiseInvalidChoice():
-    def __enter__(self):
-        pass
-    def __exit__(self, exc_type, exc_value, exc_traceback):
-        if exc_type == ValueError:
-            raise InvalidChoice()
-        return False
-
 def choose_one(menu, choice):
     num = 0
     
@@ -56,33 +58,37 @@ def choose_one(menu, choice):
     choice_item = menu[num]
     return choice_item
 
+def expand_range(choice):
+    bounds = choice.split('-')
+    
+    if len(bounds) != 2:
+        raise InvalidChoice()
+    
+    _start = int(bounds[0])
+    _end = int(bounds[1])
+    swapped = False
+    
+    if _start > _end:
+        swapped = True
+        _temp = _start
+        _start = _end
+        _end = _temp
+    
+    nums = range(_start, _end+1, 1)
+    if not swapped:
+        nums = reversed(nums)
+    
+    return nums
+
 def expand_ranges(choices):
     _choices = choices.copy()
     for index, choice in enumerate(_choices):
         if '-' in choice:
-            bounds = choice.split('-')
-
-            if len(bounds) != 2:
-                raise InvalidChoice()
-
             del choices[index]
             
-            _start = int(bounds[0])
-            _end = int(bounds[1])
-            swapped = False
-            
-            if _start > _end:
-                swapped = True
-                _temp = _start
-                _start = _end
-                _end = _temp
-            
-            nums = range(_start, _end+1, 1)
-            if not swapped:
-                nums = reversed(nums)
-                
-            for n in nums:
-                choices.insert(index, n)
+            expanded_range = expand_range(choice)
+            for num in expanded_range:
+                choices.insert(index, num)
 
 def choose_multiple(menu, choice):
     choices = choice.split()
@@ -99,39 +105,134 @@ def choose_multiple(menu, choice):
     choice_items = [menu[num] for num in choices_num]
     return choice_items
 
+def choose_loop(menu, multiple):
+    choice = ""
+    try:
+        choice = input(f'\033[31m{">>".ljust(80, " ")}\033[0m\r\033[3C')
+    except KeyboardInterrupt:
+        end()
+        
+    if choice == "":
+        raise InvalidChoice()
+    
+    if choice == "quit":
+        end()
+        
+    result = None
+
+    if not multiple:
+        result = choose_one(menu, choice)
+    else:
+        result = choose_multiple(menu, choice)
+    
+    return result
+
 def choose(menu, multiple=False, fancy_menu=None):
-    if fancy_menu:
-        if len(menu) != len(fancy_menu):
-            raise Exception
+    if fancy_menu and len(menu) != len(fancy_menu):
+        raise Exception
 
     print_menu(fancy_menu or menu)
     
     while True:
-        choice = ""
         try:
-            choice = input(f'\033[31m{">>".ljust(80, " ")}\033[0m\r\033[3C')
-        except KeyboardInterrupt:
-            end()
-            
-        if choice == "":
-            resetLine()
-            continue
-        
-        if choice == "quit":
-            end()
-            
-        result = None
-
-        try:
-            if not multiple:
-                result = choose_one(menu, choice)
-            else:
-                result = choose_multiple(menu, choice)
+            return choose_loop(menu, multiple)
         except InvalidChoice:
             resetLine()
             continue
-        else:
-            return result
+
+# SONG PLAYING
+
+def spinner_gen():
+    while True:
+        yield('⠙')
+        yield('⠸')
+        yield('⠴')
+        yield('⠦')
+        yield('⠇')
+        yield('⠋')
+
+def format_time(duration):
+    seconds = duration % 60
+    minutes = floor(duration / 60) % 60
+    hours = floor(minutes / 60)
+    
+    seconds_str = str(seconds).rjust(2, '0')
+    
+    if hours > 0:
+        minutes_str = str(minutes).rjust(2, '0')
+    else:
+        minutes_str = str(minutes)
+        
+    hours_str = str(hours)
+    
+    time = ""
+    
+    if hours > 0:
+        time = f'{hours_str}:{minutes_str}:{seconds_str}'
+    else:
+        time = f'{minutes_str}:{seconds_str}'
+    
+    return time
+
+def get_duration(song):
+    return int(MP3(song).info.length)
+
+def get_playing_str(song):
+    return f'{format_time(get_duration(song)).ljust(7, " ")} ' +song.split('\\')[-1][0:-4]
+
+def play_song(song, height, remaining_time):
+    mixer.music.load(song)
+    mixer.music.play()
+    
+    elapsed_time = 0
+    up = '\033[A' * height
+    down = '\n' * height
+    
+    while mixer.music.get_busy():
+        try:
+            elapsed_time_str = format_time(elapsed_time)
+            remaining_time_str = format_time(remaining_time - elapsed_time)
+            print(f'{up}\r\033[41m\033[{width - len(elapsed_time_str)}C{elapsed_time_str}{down}\033[0m\r{remaining_time_str}', end='')
+
+            cursor.hide()
+            time.sleep(1)
+            elapsed_time += 1
+        except KeyboardInterrupt:
+            end()
+            
+    last_song_in_queue = height == 1
+    if last_song_in_queue:
+        end()
+
+def play_songs(songs, shuffle):    
+    _songs = songs
+    if shuffle:
+        random.shuffle(_songs)
+    
+    after = '\033[48;5;235;2m'
+    current = '\033[41m'
+    before = '\033[48;5;240m'
+    
+    
+    for song in _songs:
+        print(f'{before}{get_playing_str(song).ljust(width, " ")}')
+        
+    for index, song in enumerate(_songs):
+        
+        height = len(_songs) - index
+        up = '\033[A' * height
+        down = '\n' * height
+        print(f'\r{up}{current}{get_playing_str(song).ljust(width, " ")}{down}\r\033[0m', end='')
+        
+        remaining_time = 0
+        for i in range(index, len(_songs)):
+            remaining_time += get_duration(_songs[i])
+            
+        play_song(song, height, remaining_time)
+        
+        print(f'\r{up}{after}{get_playing_str(song).ljust(width, " ")}\033[22m{down}\r', end='')
+
+# PROGRAM FLOW
 
 def get_subfolders(directory):
     subfolders = [f.path for f in os.scandir(directory) if f.is_dir()]
@@ -165,92 +266,6 @@ def select_shuffle():
         return True
     elif choice == no:
         return False
-
-def spinner_gen():
-    while True:
-        yield('⠙')
-        yield('⠸')
-        yield('⠴')
-        yield('⠦')
-        yield('⠇')
-        yield('⠋')
-
-def play_song(song, height):
-    mixer.music.load(song)
-    mixer.music.play()
-    
-    elapsed_time = 0
-    up = '\033[A' * height
-    down = '\n' * height
-    
-    spinner = spinner_gen()
-    
-    while mixer.music.get_busy():
-        try:
-            t = format_time(elapsed_time)
-            print(f'{up}\r\033[41m\033[{width - len(t)}C{t}{down}\033[0m\r', end='')
-            cursor.hide()
-
-            time.sleep(1)
-            elapsed_time += 1
-        except KeyboardInterrupt:
-            end()
-            
-    last_song_in_queue = height == 1
-    if last_song_in_queue:
-        end()
-            
-def format_time(duration):
-    seconds = duration % 60
-    minutes = floor(duration / 60) % 60
-    hours = floor(minutes / 60)
-    
-    seconds_str = str(seconds).rjust(2, '0')
-    
-    if hours > 0:
-        minutes_str = str(minutes).rjust(2, '0')
-    else:
-        minutes_str = str(minutes)
-        
-    hours_str = str(hours)
-    
-    time = ""
-    
-    if hours > 0:
-        time = f'{hours_str}:{minutes_str}:{seconds_str}'
-    else:
-        time = f'{minutes_str}:{seconds_str}'
-    
-    return time
-
-def get_playing_str(song):
-    return f'{format_time(int(MP3(song).info.length)).ljust(7, " ")} ' +song.split('\\')[-1][0:-4]
-
-def play_songs(songs, shuffle):    
-    _songs = songs
-    if shuffle:
-        random.shuffle(_songs)
-    
-    after = '\033[48;5;235;2m'
-    current = '\033[41m'
-    before = '\033[48;5;240m'
-    
-    
-    for song in _songs:
-        print(f'{before}{get_playing_str(song).ljust(width, " ")}')
-        
-    for index, song in enumerate(_songs):
-        
-        height = len(_songs) - index
-        up = '\033[A' * height
-        down = '\n' * height
-        print(f'\r{up}{current}{get_playing_str(song).ljust(width, " ")}{down}\r\033[0m', end='')
-        
-        play_song(song, height)
-        
-        print(f'\r{up}{after}{get_playing_str(song).ljust(width, " ")}\033[22m{down}\r', end='')
-
-uv_folder_path = 'C:\\Users\\Teo\\Documents\\UV'
 
 def main():
     os.system('title uv')
